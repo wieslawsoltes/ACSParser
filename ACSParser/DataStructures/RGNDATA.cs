f370524865e108a50371bb700657de9ae09dd1b8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-13 11:31:29 +0100

    Update RGNDATA.cs

--- ACSParser/DataStructures/RGNDATA.cs
+++ ACSParser/DataStructures/RGNDATA.cs
@@ -2,4 +2,20 @@ using System.Runtime.InteropServices;
 
 namespace ACSParser;
 
-// TODO: RGNDATA
+[StructLayout(LayoutKind.Sequential)]
+public struct RGNDATA
+{
+    public RGNDATAHEADER Header;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+    public RECT[] Rectangles;
+
+    public static RGNDATA Parse(BinaryReader reader)
+    {
+        RGNDATA data = new RGNDATA();
+
+        data.Header = reader.ReadStruct<RGNDATAHEADER>();
+        data.Rectangles = reader.ReadStructs<RECT>((int)data.Header.nCount);
+
+        return data;
+    }
+}
