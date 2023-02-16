116c875d9bcf478c204c4026459cec006ad6358e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 23:35:37 +0100

    Update Program.cs

--- ACSParser/Program.cs
+++ ACSParser/Program.cs
@@ -22,80 +22,150 @@ class Program
         //Parse(@"c:\Users\Administrator\Documents\GitHub\Acdparser\clippitMS\CLIPPIT.ACS");
     }
 
-    private static void Parse(string acsFile)
+    private static ACS? Parse(string acsFile)
     {
         if (!File.Exists(acsFile))
         {
             Console.WriteLine("File not found: {0}", acsFile);
-            return;
+            return null;
         }
 
         Console.WriteLine($"ACS File: {acsFile}");
+
+        ACS acs = new ACS();
 
         try
         {
             using var input = File.OpenRead(acsFile);
             using var reader = new BinaryReader(input);
-            
-            
+
+            //
+            // ACSHEADER
+            //
             Console.WriteLine($"Position: {reader.BaseStream.Position}");
             var header = ACSHEADER.Parse(reader);
+            acs.Header = header;
             Console.WriteLine($"Position: {reader.BaseStream.Position}");
             Console.WriteLine("ACS File Header:");
             Console.WriteLine("Signature: 0x{0:X8}", header.Signature);
 
+            //
+            // Debug: ACSCHARACTERINFO
+            //
+            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            //Console.WriteLine($"Character Info");
+            //var characterInfoStyles = CharacterInfoStyles();
+            //Util.PrintMemory(input, reader, header.CharacterInfo.Offset, 41, characterInfoStyles);
+            //Console.WriteLine("");
+            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
             
-            Console.WriteLine($"Position: {reader.BaseStream.Position}");
-            Console.WriteLine($"Character Info");
-            var characterInfoStyles = CharacterInfoStyles();
-            Util.PrintMemory(input, reader, header.CharacterInfo.Offset, 41, characterInfoStyles);
-            Console.WriteLine("");
-            Console.WriteLine($"Position: {reader.BaseStream.Position}");
-            
-            
-            Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            //
+            // Debug: ACSCHARACTERINFO
+            //
+            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
             Console.WriteLine($"Character Info Location: {header.CharacterInfo.Offset:X8} (size {header.CharacterInfo.Size} bytes)");
             //Util.PrintMemory(input, reader, header.CharacterInfo.Offset, 160, characterInfoStyles);
-            Util.PrintMemory(input, reader, header.CharacterInfo, characterInfoStyles);
-            Console.WriteLine("");
+            //Util.PrintMemory(input, reader, header.CharacterInfo, characterInfoStyles);
+            //Console.WriteLine("");
  
-            
-            // ACSCHARACTERINFO
+            //
+            // Read: ACSCHARACTERINFO
+            //
             Console.WriteLine($"Position: {reader.BaseStream.Position}");
             input.Seek(header.CharacterInfo.Offset, SeekOrigin.Begin);
             Console.WriteLine($"Position: {reader.BaseStream.Position}");
             var characterInfo = ACSCHARACTERINFO.Parse(reader);
+            acs.CharacterInfo = characterInfo;
             Console.WriteLine($"Position: {reader.BaseStream.Position}");
 
+            //
+            // Debug: ACSANIMATIONINFO[]
+            //
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            Console.WriteLine($"Animation Info List Location: {header.AnimationInfoList.Offset:X8} (size {header.AnimationInfoList.Size} bytes)");
+            Util.PrintMemory(input, reader, header.AnimationInfoList);
+            Console.WriteLine("");
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
 
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
-            //Console.WriteLine($"Animation Info List Location: {header.AnimationInfoList.Offset:X8} (size {header.AnimationInfoList.Size} bytes)");
-            //Util.PrintMemory(input, reader, header.AnimationInfoList);
-            //Console.WriteLine("");
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            //
+            // Read: ACSANIMATIONINFO[]
+            //
+            input.Seek(header.AnimationInfoList.Offset, SeekOrigin.Begin);
+            ULONG animationInfoCount = reader.ULONG();
+            acs.AnimationInfo = new ACSANIMATIONINFO[animationInfoCount];
+            for (var i = 0; i < animationInfoCount; i++)
+            {
+                acs.AnimationInfo[i] = ACSANIMATIONINFO.Parse(reader);
+            }
 
+            acs.Animations = new ANIMATION[animationInfoCount];
+            for (var i = 0; i < acs.AnimationInfo.Length; i++)
+            {
+                input.Seek(acs.AnimationInfo[i].AnimationLocator.Offset, SeekOrigin.Begin);
+                acs.Animations[i] = ANIMATION.Parse(reader);
+            }
 
+            //
+            // Debug: ACSIMAGEINFO[]
+            //
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            Console.WriteLine($"Image Info List Location: {header.ImageInfoList.Offset:X8} (size {header.ImageInfoList.Size} bytes)");
+            Util.PrintMemory(input, reader, header.ImageInfoList);
+            Console.WriteLine("");
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
 
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
-            //Console.WriteLine($"Image Info List Location: {header.ImageInfoList.Offset:X8} (size {header.ImageInfoList.Size} bytes)");
-            //Util.PrintMemory(input, reader, header.ImageInfoList);
-            //Console.WriteLine("");
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            //
+            // Read: ACSIMAGEINFO[]
+            //
+            input.Seek(header.ImageInfoList.Offset, SeekOrigin.Begin);
+            ULONG imageInfoCount = reader.ULONG();
+            acs.ImageInfo = new ACSIMAGEINFO[imageInfoCount];
+            for (var i = 0; i < imageInfoCount; i++)
+            {
+                acs.ImageInfo[i] = ACSIMAGEINFO.Parse(reader);
+            }
 
-            
-            
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
-            //Console.WriteLine($"Audio Info List Location: {header.AudioInfoList.Offset:X8} (size {header.AudioInfoList.Size} bytes)");
-            //Util.PrintMemory(input, reader, header.AudioInfoList);
-            //Console.WriteLine("");
-            //Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            acs.Images = new IMAGE[imageInfoCount];
+            for (var i = 0; i < acs.ImageInfo.Length; i++)
+            {
+                input.Seek(acs.ImageInfo[i].InfoLocation.Offset, SeekOrigin.Begin);
+                acs.Images[i] = IMAGE.Parse(reader);
+            }
 
+            //
+            // Debug: ACSAUDIOINFO[]
+            //
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
+            Console.WriteLine($"Audio Info List Location: {header.AudioInfoList.Offset:X8} (size {header.AudioInfoList.Size} bytes)");
+            Util.PrintMemory(input, reader, header.AudioInfoList);
+            Console.WriteLine("");
+            Console.WriteLine($"Position: {reader.BaseStream.Position}");
 
+            //
+            // Read: ACSAUDIOINFO[]
+            //
+            input.Seek(header.AudioInfoList.Offset, SeekOrigin.Begin);
+            ULONG audioInfoCount = reader.ULONG();
+            acs.AudioInfo = new ACSAUDIOINFO[audioInfoCount];
+            for (var i = 0; i < audioInfoCount; i++)
+            {
+                acs.AudioInfo[i] = ACSAUDIOINFO.Parse(reader);
+            }
+
+            acs.Audios = new AUDIO[audioInfoCount];
+            for (var i = 0; i < acs.AudioInfo.Length; i++)
+            {
+                input.Seek(acs.AudioInfo[i].AudioData.Offset, SeekOrigin.Begin);
+                acs.Audios[i] = AUDIO.Parse(reader);
+            }
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error: {0}", ex.Message);
+            return null;
         }
+
+        return acs;
     }
 
     private static List<Style> CharacterInfoStyles()
