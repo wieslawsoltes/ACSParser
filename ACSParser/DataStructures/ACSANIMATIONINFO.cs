3619a6b989061905ec0b8b2f431614db1ee55e63
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 23:34:10 +0100

    Update ACSANIMATIONINFO.cs

--- ACSParser/DataStructures/ACSANIMATIONINFO.cs
+++ ACSParser/DataStructures/ACSANIMATIONINFO.cs
@@ -3,14 +3,42 @@ namespace ACSParser.DataStructures;
 public struct ACSANIMATIONINFO
 {
     public STRING AnimationName;
-    public ACSLOCATOR AnimationLocator;
+    public ACSLOCATOR AnimationLocator; // 8 bytes
 
+    // TODO: Parse not tested.
     public static ACSANIMATIONINFO Parse(BinaryReader reader)
     {
         ACSANIMATIONINFO animation = new ACSANIMATIONINFO();
 
         animation.AnimationName = STRING.Parse(reader);
         animation.AnimationLocator = ACSLOCATOR.Parse(reader);
+
+        return animation;
+    }
+}
+
+public struct ANIMATION
+{
+    public STRING AnimationName;
+    public BYTE TransitionType; // 1 byte
+    public STRING ReturnAnimation;
+    public ACSFRAMEINFO[] AnimationFrames;
+
+    // TODO: Parse not tested.
+    public static ANIMATION Parse(BinaryReader reader)
+    {
+        ANIMATION animation = new ANIMATION();
+
+        animation.AnimationName = STRING.Parse(reader);
+        animation.TransitionType = reader.BYTE();
+        animation.AnimationName = STRING.Parse(reader);
+
+        USHORT animationFramesCount = reader.USHORT();
+        animation.AnimationFrames = new ACSFRAMEINFO[animationFramesCount];
+        for (var i = 0; i < animationFramesCount; i++)
+        {
+            animation.AnimationFrames[i] = ACSFRAMEINFO.Parse(reader);
+        }
 
         return animation;
     }
