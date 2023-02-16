8884efd8f462b77c9492e9531121b26d0a6fcb8d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 23:34:55 +0100

    Update LANGID.cs

--- ACSParser/DataStructures/LANGID.cs
+++ ACSParser/DataStructures/LANGID.cs
@@ -2,8 +2,9 @@ namespace ACSParser.DataStructures;
 
 public struct LANGID
 {
-    public USHORT LanguageID;
+    public USHORT LanguageID; // 2 bytes
 
+    // TODO: Parse not tested.
     public static LANGID Parse(BinaryReader reader)
     {
         LANGID langid = new LANGID();
