5b2634d2dc3eb93e23695b80b834df8349036bc2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 12:53:07 +0100

    Update Program.cs

--- ACSParser/Program.cs
+++ ACSParser/Program.cs
@@ -31,20 +31,57 @@ class Program
             Console.WriteLine("ACS File Header:");
             Console.WriteLine("Signature: 0x{0:X8}", header.Signature);
 
+
+            Console.WriteLine($"Character Info");
+            var characterInfoStyles = new List<Style>();
+            // MinorVersion
+            ULONG start = 0; ULONG end = 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Blue));
+            // MajorVersion
+            start = end; end += 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Green));
+            // LocalizedInfo
+            start = end; end += 8;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Cyan));
+            // UniqueId
+            start = end; end += 16;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Magenta));
+            // CharacterWidth
+            start = end; end += 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Blue));
+            // CharacterHeight
+            start = end; end += 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Green));
+            // TransparentColorIndex
+            start = end; end += 1;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Yellow));
+            // Flags
+            start = end; end += 4;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Red));
+            // AnimationSetMajorVersion
+            start = end; end += 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Blue));
+            // AnimationSetMinorVersion
+            start = end; end += 2;
+            characterInfoStyles.Add(new(start, end, ConsoleColor.Green));
+            PrintMemory(input, reader, header.CharacterInfo.Offset, 41, characterInfoStyles);
+            Console.WriteLine("");
+
             Console.WriteLine($"Character Info Location: {header.CharacterInfo.Offset:X8} (size {header.CharacterInfo.Size} bytes)");
-            PrintMemory(input, reader, header.CharacterInfo);
+            //PrintMemory(input, reader, header.CharacterInfo, characterInfoStyles);
+            PrintMemory(input, reader, header.CharacterInfo.Offset, 160, characterInfoStyles);
             Console.WriteLine("");
   
             Console.WriteLine($"Animation Info List Location: {header.AnimationInfoList.Offset:X8} (size {header.AnimationInfoList.Size} bytes)");
-            PrintMemory(input, reader, header.AnimationInfoList);
+            //PrintMemory(input, reader, header.AnimationInfoList);
             Console.WriteLine("");
 
             Console.WriteLine($"Image Info List Location: {header.ImageInfoList.Offset:X8} (size {header.ImageInfoList.Size} bytes)");
-            PrintMemory(input, reader, header.ImageInfoList);
+            //PrintMemory(input, reader, header.ImageInfoList);
             Console.WriteLine("");
 
             Console.WriteLine($"Audio Info List Location: {header.AudioInfoList.Offset:X8} (size {header.AudioInfoList.Size} bytes)");
-            PrintMemory(input, reader, header.AudioInfoList);
+            //PrintMemory(input, reader, header.AudioInfoList);
             Console.WriteLine("");
 
             var characterInfo = ACSCHARACTERINFO.Parse(reader);
@@ -57,11 +94,29 @@ class Program
         }
     }
 
-    private static void PrintMemory(FileStream input, BinaryReader reader, ACSLOCATOR locator)
-    {            
-        var offset = locator.Offset;
-        var size = locator.Size;
+    private class Style
+    {
+        public readonly ULONG Start;
+        public readonly ULONG End;
+        public readonly ConsoleColor Color;
 
+        public Style(uint start, uint end, ConsoleColor color)
+        {
+            Start = start;
+            End = end;
+            Color = color;
+        }
+
+        public bool Match(ULONG offset) => offset >= Start && offset < End;
+    }
+
+    private static void PrintMemory(FileStream input, BinaryReader reader, ACSLOCATOR locator, List<Style>? styles = null)
+    {
+        PrintMemory(input, reader, locator.Offset, locator.Size, styles);
+    }
+
+    private static void PrintMemory(FileStream input, BinaryReader reader, ULONG offset, ULONG size, List<Style>? styles = null)
+    {
         var currentOffset = input.Position;
         input.Seek(offset, SeekOrigin.Begin);
         var byteArray = reader.ReadBytes((int)size);
@@ -69,16 +124,23 @@ class Program
         var addressOffset = offset % 16; // Offset into 16-byte boundary
         var alignedOffset = offset - addressOffset; // Starting offset aligned to 16-byte boundary
 
-        for (int i = 0; i < byteArray.Length; i += 16)
+        var currentColor = Console.ForegroundColor;
+        
+        for (var i = 0; i < byteArray.Length; i += 16)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.Write($"{(i + alignedOffset):X8}: ");
-            for (int j = i; j < i + 16; j++)
+            Console.ForegroundColor = currentColor;
+
+            for (var j = i; j < i + 16; j++)
             {
                 if (j < addressOffset || j >= addressOffset + byteArray.Length)
                 {
                     if ((j + 1 - alignedOffset) % 8 == 0)
                     {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.Write("   | ");
+                        Console.ForegroundColor = currentColor;
                     }
                     else
                     {
@@ -87,20 +149,41 @@ class Program
                 }
                 else
                 {
-                    Console.Write($"{byteArray[j - addressOffset]:X2} ");
+                    var byteOffset = j - addressOffset;
+                    var style = styles?.FirstOrDefault(x => x.Match((ULONG)byteOffset));
+                    Console.ForegroundColor = style?.Color ?? ConsoleColor.Black;
+                    Console.Write($"{byteArray[byteOffset]:X2} ");
+                    Console.ForegroundColor = currentColor;
+
                     if ((j + 1 - alignedOffset) % 8 == 0)
                     {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.Write("| ");
+                        Console.ForegroundColor = currentColor;
                     }
                 }
             }
+
             Console.Write(" ");
+
             for (int j = i; j < i + 16; j++)
             {
                 if (j < byteArray.Length + addressOffset && j >= addressOffset)
                 {
-                    char c = (char)byteArray[j - addressOffset];
-                    Console.Write(Char.IsControl(c) ? ' ' : c);
+                    var byteOffset = j - addressOffset;
+                    char c = (char)byteArray[byteOffset];
+
+                    if (Char.IsControl(c))
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        var style = styles?.FirstOrDefault(x => x.Match((ULONG)byteOffset));
+                        Console.ForegroundColor = style?.Color ?? ConsoleColor.DarkGray;
+                        Console.Write(c); 
+                        Console.ForegroundColor = currentColor;
+                    }
                 }
                 else
                 {
