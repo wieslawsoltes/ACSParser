e108fe0235b29bd9affb829b59c6151d9e2e62e0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 23:34:40 +0100

    Update BITMAPINFOHEADER.cs

--- ACSParser/DataStructures/BITMAPINFOHEADER.cs
+++ ACSParser/DataStructures/BITMAPINFOHEADER.cs
@@ -2,18 +2,19 @@ namespace ACSParser.DataStructures;
 
 public struct BITMAPINFOHEADER
 {
-    public ULONG Size;
-    public LONG Width;
-    public LONG Height;
-    public USHORT Planes;
-    public USHORT BitCount;
-    public ULONG Compression;
-    public ULONG SizeImage;
-    public LONG XPelsPerMeter;
-    public LONG YPelsPerMeter;
-    public ULONG ClrUsed;
-    public ULONG ClrImportant;
+    public ULONG Size; // 4 bytes
+    public LONG Width; // 4 bytes
+    public LONG Height; // 4 bytes
+    public USHORT Planes; // 2 bytes
+    public USHORT BitCount; // 2 bytes
+    public ULONG Compression; // 4 bytes
+    public ULONG SizeImage; // 4 bytes
+    public LONG XPelsPerMeter; // 4 bytes
+    public LONG YPelsPerMeter; // 4 bytes
+    public ULONG ClrUsed; // 4 bytes
+    public ULONG ClrImportant; // 4 bytes
 
+    // TODO: Parse not tested.
     public static BITMAPINFOHEADER Parse(BinaryReader reader)
     {
         BITMAPINFOHEADER header = new BITMAPINFOHEADER();
