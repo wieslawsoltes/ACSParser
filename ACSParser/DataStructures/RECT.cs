0011ad65357ef6dac6bb8ee8057e6a001052fc07
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-16 23:35:06 +0100

    Update RECT.cs

--- ACSParser/DataStructures/RECT.cs
+++ ACSParser/DataStructures/RECT.cs
@@ -2,11 +2,12 @@ namespace ACSParser.DataStructures;
 
 public struct RECT
 {
-    public LONG Left;
-    public LONG Top;
-    public LONG Right;
-    public LONG Bottom;
+    public LONG Left; // 4 bytes
+    public LONG Top; // 4 bytes
+    public LONG Right; // 4 bytes
+    public LONG Bottom; // 4 bytes
 
+    // TODO: Parse not tested.
     public static RECT Parse(BinaryReader reader)
     {
         RECT rect = new RECT();
