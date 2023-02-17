69b84529b1ca7d0fb04184cbd93fad9cbb4b4463
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-17 23:27:46 +0100

    Update Decompressor.cs

--- ACSParser/Decompressor.cs
+++ ACSParser/Decompressor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace ACSParser;
 
@@ -35,6 +36,8 @@ public static class Decompressor
         var bitPosition = 0;
         var insertionPoint = 0;
 
+Debug(bits, 0, 7, "Header");
+
         // Header 0x00
         bitPosition += 8;
 
@@ -51,15 +54,20 @@ public static class Decompressor
             [6] = 1, [9] = 65, [12] = 577, [20] = 2673
         };
 
+
         while (bitPosition < dataLength)
         {
+var startPosition = bitPosition;
+
             var firstBitOfSequence = !bits[bitPosition];
             bitPosition += 1;
 
             if (firstBitOfSequence)
             {
+                Debug(bits, startPosition, bitPosition + 7, "Uncompressed");
+
                 // Uncompressed Byte
-                var data = GetByte(bits, bitPosition + 1);
+                var data = GetByte(bits, bitPosition);
                 results[insertionPoint] = data;
                 // startBit + data = 9 bits
                 bitPosition += 8;
@@ -82,7 +90,7 @@ public static class Decompressor
                         break;
                     }
                 }
-                bitPosition += numOffsetSeqBits < 3 ? numOffsetSeqBits + 1 : numOffsetSeqBits;
+                bitPosition += numOffsetSeqBits == 3 ? numOffsetSeqBits : numOffsetSeqBits + 1;
 
                 var nextValueSizeInBits = nextValueSizeBits[numOffsetSeqBits];
 
@@ -99,11 +107,14 @@ public static class Decompressor
                 // If the bit count is 20:
                 if (nextValueSizeInBits == 20 && byteOffsetInResult == 0x000FFFFF)
                 {
+Debug(bits, bitPosition, bitPosition + nextValueSizeInBits, "End of Stream");
                     bitPosition += nextValueSizeInBits;
                     // the end of the bit stream has been reached
                     break;
                 }
-                bitPosition += nextValueSizeInBits + 1;
+//bitPosition += nextValueSizeInBits + 1;
+bitPosition += nextValueSizeInBits;
+//System.Diagnostics.Debug.WriteLine($"nextValueSizeInBits={nextValueSizeInBits}");
                 byteOffsetInResult += valueToAdd[nextValueSizeInBits];
 
                 // Following the offset bits, count the number of sequential bits which have a value of 1. The maximum number of bits is 11.
@@ -113,27 +124,34 @@ public static class Decompressor
                 var sequenceCount = 0;
                 for (; sequenceCount < 11; sequenceCount++)
                 {
-                    if (bits[bitPosition + sequenceCount])
+//System.Diagnostics.Debug.WriteLine($"{bitPosition }={(bits[bitPosition] ? '1' : '0')}");
+                    if (bits[bitPosition])
                     {
                         numDecodedByteSeqBits++;
                         remainingBitsInLastSequence++;
+                        bitPosition += 1;
                     }
                     else
                     {
+                        bitPosition += 1;
                         break;
                     }
                 }
 
-                if (sequenceCount == 10 && bits[bitPosition + 1])
+                if (sequenceCount == 10 && bits[bitPosition])
                 {
                     throw new Exception("Error invalid sequence");
                 }
 
-                for (var i = 0; i < remainingBitsInLastSequence; i++)
+                if (remainingBitsInLastSequence > 0)
                 {
-                    if (bits[bitPosition + i])
+                    for (var i = 0; i < remainingBitsInLastSequence; i++)
                     {
-                        numDecodedByteSeqBits++;
+                        if (bits[bitPosition])
+                        {
+                            numDecodedByteSeqBits++;
+                        }
+                        bitPosition += 1;
                     }
                 }
 
@@ -149,12 +167,33 @@ public static class Decompressor
                 // incrementing the insertion point after each BYTE,
                 // as the copying may overlap past the original insertion point
 
+var endPosition = bitPosition - 1;
+Debug(bits, startPosition, endPosition, $"Compressed, Offset={byteOffsetInResult}, Bytes={bytesDecodedInSequence}, remainingBitsInLastSequence={remainingBitsInLastSequence}");
+/*
+00000010000000000010000000001000
+00001011000010010000000101000010
+10110111000110011000000011101101
+11111111111111111111111
+ 
+00000000 [0..7] Header
+000000100 [8..16] Uncompressed
+000000000 [17..25] Uncompressed
+100000000 [26..34] Compressed
+010000000 [35..43] Uncompressed
+10110000100 [44..54]
+10000000101 [55..
+000010101
+1011100011001
+100000001110110
+111111111111111111111111 End of stream
+ */
+                
                 for (var i = 0; i < bytesDecodedInSequence; i++)
                 {
                     var sourcePoint = insertionPoint - byteOffsetInResult;
                     if (sourcePoint > results.Length - 1 || sourcePoint < 0)
                     {
-                        throw new Exception("Invalid offset.")
+                        throw new Exception("Invalid offset.");
                     }
                     results[insertionPoint] = results[sourcePoint];
                     insertionPoint += 1;
@@ -171,5 +210,17 @@ public static class Decompressor
         }
         
         return results;
+    }
+
+    private static void Debug(BitArray bits, int startPosition, int endPosition, string message)
+    {
+        var bitString = "";
+
+        for (var i = startPosition; i <= endPosition; i++)
+        {
+            bitString += bits[i] ? "1" : "0";
+        }
+
+        System.Diagnostics.Debug.WriteLine($"{bitString} [{startPosition}..{endPosition}] {message}");
     }
 }
