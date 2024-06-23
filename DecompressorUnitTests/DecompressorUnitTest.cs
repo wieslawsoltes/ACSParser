1aa75e69a6e88d46bfb90b7357acb20a15a2cbb4
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2024-06-23 18:06:34 +0800

    Revert "delete old tests"
    This reverts commit b343cc7ee8b84a72e86087b2bc5fee946fb748b1.

--- DecompressorUnitTests/DecompressorUnitTest.cs
+++ DecompressorUnitTests/DecompressorUnitTest.cs
@@ -6,6 +6,130 @@ namespace DecompressorUnitTests;
 public class DecompressorUnitTest
 {
     [Fact]
+    public void CountSeqBits_0()
+    {
+        var bits = new BitArray(new[] { 0b00000000 });
+        var bitPosition = 0;
+        var numOffsetSeqBits = Decompressor.CountSeqBits(bits, ref bitPosition);
+        var nextValueSizeInBits = Decompressor.NextValueSizeBits[numOffsetSeqBits];
+
+        Assert.Equal(0, numOffsetSeqBits);
+        Assert.Equal(1, bitPosition);
+        Assert.Equal(6, nextValueSizeInBits);
+    }
+
+    [Fact]
+    public void CountSeqBits_1()
+    {
+        var bits = new BitArray(new[] { 0b00000001 });
+        var bitPosition = 0;
+        var numOffsetSeqBits = Decompressor.CountSeqBits(bits, ref bitPosition);
+        var nextValueSizeInBits = Decompressor.NextValueSizeBits[numOffsetSeqBits];
+
+        Assert.Equal(1, numOffsetSeqBits);
+        Assert.Equal(2, bitPosition);
+        Assert.Equal(9, nextValueSizeInBits);
+    }
+
+    [Fact]
+    public void CountSeqBits_2()
+    {
+        var bits = new BitArray(new[] { 0b00000011 });
+        var bitPosition = 0;
+        var numOffsetSeqBits = Decompressor.CountSeqBits(bits, ref bitPosition);
+        var nextValueSizeInBits = Decompressor.NextValueSizeBits[numOffsetSeqBits];
+        
+        Assert.Equal(2, numOffsetSeqBits);
+        Assert.Equal(3, bitPosition);
+        Assert.Equal(12, nextValueSizeInBits);
+    }
+
+    [Fact]
+    public void CountSeqBits_3()
+    {
+        var bits = new BitArray(new[] { 0b00000111 });
+        var bitPosition = 0;
+        var numOffsetSeqBits = Decompressor.CountSeqBits(bits, ref bitPosition);
+        var nextValueSizeInBits = Decompressor.NextValueSizeBits[numOffsetSeqBits];
+        
+        Assert.Equal(3, numOffsetSeqBits);
+        Assert.Equal(3, bitPosition);
+        Assert.Equal(20, nextValueSizeInBits);
+    }
+
+    [Fact]
+    public void GetInt32()
+    {
+        var bits = new BitArray(new byte[] { 0b00000001, 0b00000000, 0b00000000, 0b00000000 });
+        var value = Decompressor.GetInt32(bits, 0, 32);
+        Assert.Equal(1, value);
+    }
+
+    [Fact]
+    public void GetInt32_6_bits()
+    {
+        var nextValueSizeInBits = 6;
+        var bits = new BitArray(new byte[] { 0b00000000, 0b00000000, 0b00000000 });
+        var byteOffsetInResult = Decompressor.GetInt32(bits, 0, nextValueSizeInBits);
+
+        Assert.Equal(0, byteOffsetInResult);
+
+        var valueToAdd = Decompressor.ValueToAdd[nextValueSizeInBits];
+        Assert.Equal(1, valueToAdd);
+
+        byteOffsetInResult += valueToAdd;
+        Assert.Equal(1, valueToAdd);
+    }
+
+    [Fact]
+    public void GetInt32_9_bits()
+    {
+        var nextValueSizeInBits = 9;
+        var bits = new BitArray(new byte[] { 0b00000000, 0b00000000, 0b00000000 });
+        var byteOffsetInResult = Decompressor.GetInt32(bits, 0, nextValueSizeInBits);
+
+        Assert.Equal(0, byteOffsetInResult);
+
+        var valueToAdd = Decompressor.ValueToAdd[nextValueSizeInBits];
+        Assert.Equal(65, valueToAdd);
+
+        byteOffsetInResult += valueToAdd;
+        Assert.Equal(65, valueToAdd);
+    }
+
+    [Fact]
+    public void GetInt32_12_bits()
+    {
+        var nextValueSizeInBits = 12;
+        var bits = new BitArray(new byte[] { 0b00000000, 0b00000000, 0b00000000 });
+        var byteOffsetInResult = Decompressor.GetInt32(bits, 0, nextValueSizeInBits);
+
+        Assert.Equal(0, byteOffsetInResult);
+
+        var valueToAdd = Decompressor.ValueToAdd[nextValueSizeInBits];
+        Assert.Equal(577, valueToAdd);
+
+        byteOffsetInResult += valueToAdd;
+        Assert.Equal(577, valueToAdd);
+    }
+
+    [Fact]
+    public void GetInt32_20bits()
+    {
+        var nextValueSizeInBits = 20;
+        var bits = new BitArray(new byte[] { 0b00000000, 0b00000000, 0b00000000 });
+        var byteOffsetInResult = Decompressor.GetInt32(bits, 0, nextValueSizeInBits);
+
+        Assert.Equal(0, byteOffsetInResult);
+
+        var valueToAdd = Decompressor.ValueToAdd[nextValueSizeInBits];
+        Assert.Equal(4673, valueToAdd);
+
+        byteOffsetInResult += valueToAdd;
+        Assert.Equal(4673, valueToAdd);
+    }
+
+    [Fact]
     public void Sample()
     {
         byte[] compressedData = 
